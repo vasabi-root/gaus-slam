4d6f7d8eda01708645758a63d1da1ed1b6880367
bashmac.wsl <vasabi.ph@gmail.com>
2025-10-08 04:36:08 +0300

    gen_video: set flexible settings for camera lock

--- scripts/gen_video.py
+++ scripts/gen_video.py
@@ -1,6 +1,7 @@
 
 import sys
 import os
+from pathlib import Path
 
 _BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, _BASE_DIR)
@@ -23,6 +24,32 @@ from datasets import load_dataset_config, get_dataset
 from render import Renderer_view
 from open3d_ui import Vis_Mesh
 from evo.core.trajectory import PosePath3D
+
+import numpy as np
+
+def get_cam_loc_from_traj(config: {}) -> np.ndarray:
+    dataset_dir = Path(config["data"]["basedir"])
+    scene_dir = dataset_dir / config["data"]["sequence"]
+    
+    traj_txt = scene_dir / 'traj_cam_from_world.txt'
+    assert os.path.exists(traj_txt)
+    
+    with open(traj_txt, 'r') as f:
+        lines = []
+        for line in f:
+            lines.append(line)
+    
+    middle_cam_str = lines[len(lines) // 2]
+    middle_cam_nums = list(map(float, middle_cam_str.split()))
+    
+    assert len(middle_cam_nums) == 16
+    
+    middle_cam_array = np.array(middle_cam_nums).reshape((4, 4))
+    middle_cam_array = np.array([[1, 0, 0, 0], 
+                                 [0, 1, 0, 0], 
+                                 [0, 0, 1, 0.7], 
+                                 [0, 0, 0, 1]]) @ middle_cam_array
+    return middle_cam_array
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -65,14 +92,11 @@ if __name__ == "__main__":
         use_train_split=dataset_config["use_train_split"],
     )
     
-    config['viz']['mesh_every'] = 5
+    config['viz']['mesh_every'] = 2
     config['viz']['gen_animation'] = True
-    config['viz']['video_freq'] = 15
-    # scannetpp b20
-    config['viz']['cam_loc'] = ([[ -0.02, -1.0,  0.0,-3.5],
-                                 [-1.0, 0.02, -0.0, 2.95],
-                                 [ 0.0, -0.0, -1.0, 6.94],
-                                 [ 0.0,  0.0,  0.0, 1.0 ]])   
+    config['viz']['video_freq'] = 2
+    
+    config['viz']['cam_loc'] = get_cam_loc_from_traj(config)
 
     viz = Vis_Mesh(config)
 
@@ -87,7 +111,7 @@ if __name__ == "__main__":
         render_color = torch.clamp(render_pkg['render_color'], 0.0, 1.0)
         render_depth = render_pkg['render_depth']
         
-        if time_idx % 5 == 0:
+        if time_idx % 2 == 0:
             viz.update_frame(render_color, render_depth, pred_w2c, gt_w2c, time_idx)
     
     viz.destroy()
